df524b71b36be84fa0e9d8b74a8bff09487e1ea8
adit-syntax <aditsyntax@gmail.com>
2024-10-21 05:08:03 +0530

    added icon, title and payer

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((800, 600))
+
+
+pygame.display.set_caption("Space Invaders")
+icon=pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player(x,y):
+    screen.blit(playerImg, (x,y))
+
+
+
+running = True
+while running:
+
+
+    screen.fill((0, 0, 0))
+    playerX+= 0.1
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running = False
+
+
+    player(playerX, playerY)
+    pygame.display.update()
