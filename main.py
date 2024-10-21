18d27abab646f0ca8b7b9e0085645c9cf9671e86
adit-syntax <aditsyntax@gmail.com>
2024-10-21 06:49:13 +0530

    added background image

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+background=pygame.image.load('background.png')
 
 pygame.display.set_caption("Space Invaders")
 icon=pygame.image.load('icon.png')
@@ -20,7 +21,7 @@ playerX_change = 0
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0,800)
 enemyY = random.randint(50,150)
-enemyX_change = 0.3
+enemyX_change = 4
 enemyY_change = 40
 
 
@@ -37,6 +38,8 @@ while running:
 
     screen.fill((0, 0, 0))
 
+    screen.blit(background, (0, 0))
+    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running = False
@@ -44,9 +47,9 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -64,10 +67,10 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-            enemyX_change = 0.3
+            enemyX_change = 4
             enemyY += enemyY_change
     elif enemyX >= 736:
-            enemyX_change =-0.3
+            enemyX_change =-4
             enemyY += enemyY_change
 
     player(playerX, playerY)
