7cbdbb863a2942613ee2d8bf8b87716dadad4a6d
adit-syntax <aditsyntax@gmail.com>
2024-10-21 06:32:56 +0530

    added enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 pygame.init()
 
@@ -17,10 +17,17 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0
+
+
 def player(x,y):
     screen.blit(playerImg, (x,y))
 
-
+def enemy(x,y):
+    screen.blit(enemyImg, (x,y))
 
 running = True
 while running:
@@ -53,4 +60,5 @@ while running:
         playerX = 736
 
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
